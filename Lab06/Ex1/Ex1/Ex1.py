55baef9e7c96e83244f5b730f835434791a54788
RaulDan2001 <116735505+RaulDan2001@users.noreply.github.com>
2024-03-25 00:00:09 +0200

    Add files via upload

---
+++ Lab06/Ex1/Ex1/Ex1.py
@@ -0,0 +1,15 @@
+﻿import re
+
+regex = r'0*1(0+10*1)*'
+
+test_strings = ['0', '1', '01', '011', '0011', '00111', '010111']
+
+
+for test_string in test_strings:
+    match = re.match(regex, test_string)
+    if match:
+        print(f'Stringul "{test_string}" se potrivește cu expresia regulată.')
+    else:
+        print(f'Stringul "{test_string}" nu se potrivește cu expresia regulată.')
+
+
---
+++ Lab06/Ex3/Ex3/Ex3.py
@@ -0,0 +1,16 @@
+﻿import re
+
+def validate_postal_code(postal_code):
+    pattern = r'^[10-90]{2}[0-9]{3}$'
+    if re.match(pattern, postal_code):
+        return True
+    else:
+        return False
+
+# Exemplu de utilizare:
+postal_code = input("Introduceți codul poștal: ")
+if validate_postal_code(postal_code):
+    print("Codul poștal este valid.")
+else:
+    print("Codul poștal nu este valid.")
+
